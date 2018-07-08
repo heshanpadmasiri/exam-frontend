83f48f16dcfde8407c57726c4ac97479f3b563fc
Heshan Padmasiri <heshanpadmasiri@hotmail.com>
2018-07-08 23:52:25 +0530

    Blocking recorrection requests

--- src/app/components/re-correction/re-correction.component.ts
+++ src/app/components/re-correction/re-correction.component.ts
@@ -13,15 +13,19 @@ export class ReCorrectionComponent implements OnInit {
   selectedModule: string;
   result: string;
   reason: string;
-  constructor(private userService: UserServicesService,            // to do the dependency injection when component is constructed further use
+  block: boolean;
+  btnText: string;
+
+  constructor(private userService: UserServicesService,  // to do the dependency injection when component is constructed further use
               private flashMessageService: FlashMessagesService) {
     this.registeredModules = [];
   }
 
   ngOnInit() {                                           // is component life cyclehook  It's a good place to put initialization logic.
+    this.unBlockSubmission();
     this.userService.getRegisteredModules().subscribe(message => {
       // observal pattern
-      if (message.success) { 
+      if (message.success) {
         this.registeredModules = message.msg;
         this.selectedModule = this.registeredModules[0];
         console.log(this.registeredModules);
@@ -29,7 +33,18 @@ export class ReCorrectionComponent implements OnInit {
     });
   }
 
+  blockSubmission() {
+    this.block = true;
+    this.btnText = 'Please wait';
+  }
+
+  unBlockSubmission() {
+    this.block = false;
+    this.btnText = 'Submit';
+  }
+
   onSubmit() {
+    this.blockSubmission();
     this.userService.requestReCorrection(this.selectedModule).subscribe(message => {
       if (message.success) {
         this.flashMessageService.show('Re-correction request placed successfully', {
@@ -42,6 +57,7 @@ export class ReCorrectionComponent implements OnInit {
           timeOut: 5000
         });
       }
+      this.unBlockSubmission();
     });
   }
 
